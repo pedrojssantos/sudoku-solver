5fbf6da5bc6c5a1bbbcbb2870e9f8e7618b72e5d
Pedro Junio <pedrojss2100@gmail.com>
2026-07-17 17:51:26 -0300

    feat: implementa o backend inicial

---
+++ Backend/Program.cs
@@ -0,0 +1,55 @@
+using Backend.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddOpenApi();
+builder.Services.AddScoped<SolverWrapper>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("/api/sudoku/solve", (SudokuRequest req, SolverWrapper solverWpp) =>
+{
+    try
+    {
+        int[] board = req.Board;
+
+        for (int i = 0; i < 80; ++i)
+        {
+            int value = board[i];
+            
+            if (value != 0)
+            {
+                solverWpp.InsertValue(i, value);
+            }
+        }
+
+        solverWpp.SolveSudoku();
+
+        int[] solvedBoard = new int[81];
+
+        solverWpp.GetValues(ref solvedBoard[0]);
+
+        return Results.Ok(new { sudoku = solvedBoard });
+    }
+    catch(InvalidOperationException ex)
+    {
+        return Results.Problem(detail: ex.Message, statusCode: 500);
+    }
+    catch(Exception ex)
+    {
+        return Results.Problem(detail: "Something went wrong.", statusCode: 500);
+    }
+});
+
+app.Run();
+
+public record SudokuRequest(int[] Board);
---
+++ Backend/Services/SolverWrapper.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Backend.Services
+{
+    public partial class SolverWrapper : IDisposable
+    {
+        private const string SOLVER_LIB_DLL_PATH = "Solver.dll";
+
+        [LibraryImport(SOLVER_LIB_DLL_PATH)]
+        [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
+        private static partial IntPtr CreateSudokuGraph();
+
+        [LibraryImport(SOLVER_LIB_DLL_PATH)]
+        [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
+        private static partial void DisposeSudokuGraph(IntPtr sudokuGraph);
+
+        [LibraryImport(SOLVER_LIB_DLL_PATH)]
+        [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
+        private static partial void InsertValue(IntPtr sudokuGraph, int id, int value);
+
+        [LibraryImport(SOLVER_LIB_DLL_PATH)]
+        [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
+        [return: MarshalAs(UnmanagedType.U1)]
+        private static partial bool SolveSudokuGraph(IntPtr sudokuGraph);
+
+        [LibraryImport(SOLVER_LIB_DLL_PATH)]
+        [UnmanagedCallConv(CallConvs = new[] { typeof(CallConvCdecl) })]
+        private static partial void GetGraphValues(IntPtr graph, ref int buffer);
+
+        private IntPtr _sudokuGraph;
+
+        public SolverWrapper()
+        {
+            _sudokuGraph = CreateSudokuGraph();
+
+            if (_sudokuGraph == IntPtr.Zero)
+                throw new InvalidOperationException("Failed to create the graph.");
+        }
+        
+        public void InsertValue(int id, int value)
+        {
+            InsertValue(_sudokuGraph, id, value);
+        }
+
+        public bool SolveSudoku()
+        {
+            return SolveSudokuGraph(_sudokuGraph);
+        }
+
+        public void GetValues(ref int buffer)
+        {
+            GetGraphValues(_sudokuGraph, ref buffer);
+        }
+
+        public void Dispose()
+        {
+            DisposeSudokuGraph(_sudokuGraph);
+        }
+    }
+}
